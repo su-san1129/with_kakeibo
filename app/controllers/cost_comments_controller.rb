20a520ed7d296fb482da012f5b98e3636bab71d7
su-san1129 <deviationvalue1129@icloud.com>
2019-06-15 17:07:21 +0900

    [fix]comment-function

--- app/controllers/cost_comments_controller.rb
+++ app/controllers/cost_comments_controller.rb
@@ -1,18 +1,26 @@
 class CostCommentsController < ApplicationController
+
+	before_action :set_variable_cost, only: [:create, :destroy]
+
 	def create
-		variable_cost = VariableCost.find(params[:variable_cost_id])
     comment = current_user.cost_comments.new(cost_comment_params)
-    comment.variable_cost_id = variable_cost.id
+    comment.variable_cost_id = @variable_cost.id
     comment.save
-    redirect_to variable_cost_path(variable_cost)
+    redirect_to variable_cost_path(@variable_cost)
 	end
 
 	def destroy
-		
+    comment = current_user.cost_comments.find_by(variable_cost_id: @variable_cost.id)
+    comment.destroy
+    redirect_to variable_cost_path(@variable_cost)
 	end
 
 	private
 		def cost_comment_params
 		params.require(:cost_comment).permit(:user_id, :variable_cost_id, :comment)
+		end
+
+		def set_variable_cost
+			@variable_cost = VariableCost.find(params[:variable_cost_id])
 		end
 end
