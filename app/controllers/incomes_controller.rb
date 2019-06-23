b4fb3cb58e27c8e9170d805f960a892795d76cd3
su-san1129 <deviationvalue1129@icloud.com>
2019-06-23 14:23:52 +0900

    images change folder assets=>public

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -11,20 +11,6 @@ class IncomesController < ApplicationController
 			redirect_to user_path(current_user)
 		end
 	end
-		# if @income.save
-		# 	redirect_to user_path(current_user.id)
-		# else
-		# 	render "users/index"
-		# end
-		# respond_to do |format|
-		#    if @income.save
-		#      format.html { redirect_to user_path(current_user.id), notice: '入力が完了しました！' }
-		#      # format.json { render :show, status: :created, location: @variable_cost }
-		#    else
-		#      format.html { render "users/index" }
-		#      # format.json { render json: @variable_cost.errors, status: :unprocessable_entity }
-		#    end
-		#  end
 
 	private
 		def income_params
