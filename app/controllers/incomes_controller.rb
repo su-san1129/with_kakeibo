a7523ef14b4510a05c0deae1b01313b37482a1f6
su-san1129 <deviationvalue1129@icloud.com>
2019-06-26 08:52:50 +0900

    [add]intro.js

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -4,11 +4,17 @@ class IncomesController < ApplicationController
 		@income = Income.new(income_params)
 		if Income.find_by(user_id: current_user.id, payday: Time.now.all_month).present?
 			@income = Income.find_by(user_id: current_user.id)
+			# Income.find_by(user_id: current_user.id, payday: params[:payaday].month).present?
+			# @income = Income.find_by(user_id: current_user.id, payday: prams[:payday].month )
 			@income.update(income_params)
-			redirect_to user_path(current_user)
+			redirect_to user_path(current_user),notice: "給与を更新しました！"
+
 		else
-			@income.save
-			redirect_to user_path(current_user)
+			if @income.save
+				redirect_to user_path(current_user),notice: "給与を登録しました！"
+			else
+				render 'users/show',notice: "保存できませんでした。"
+			end
 		end
 	end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :trackable
 
   has_many :variable_costs, dependent: :destroy
   has_many :categories, dependent: :destroy
